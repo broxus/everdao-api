517f8170fefa892e7e3ab1ebf742b7ed98e7725e
Plank <ckiper@yandex.ru>
2022-04-19 18:24:05 +0300

    fix start server

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-use std::env;
-
 use anyhow::Result;
 
 use bridge_dao_indexer::server::*;
 
 #[tokio::main(worker_threads = 8)]
 async fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-
-    match &*args[1] {
-        "server" => start_server().await?,
-        other => anyhow::bail!("Unknown arg - {:?}", other),
-    }
-
-    Ok(())
+    start_server().await
 }
